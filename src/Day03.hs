a5714352072a33bc425e5dd82789e443953fd4c6
laracabrera <lara.cabrera@gmail.com>
2024-12-16 14:20:28 +0100

    Resuelto dia 03

--- src/Day03.hs
+++ src/Day03.hs
@@ -2,9 +2,26 @@ module Day03 where
 
 import Paths_aoc2024 (getDataFileName)
 
+import Numeric.LinearAlgebra (Matrix, fromLists, conv2, (!), rows, cols)
+
+filtradoCuarentaYDos :: Matrix Double -> [(Int, Int)]
+-- conv2 añade padding (filas y columnas adicionales, hay que ajustar los índices)
+filtradoCuarentaYDos mat =
+    [ (i-1, j-1) | i <- [2..nrows-3],
+                   j <- [2..ncols-3],
+                   let val = mat ! i ! j,
+                   val > 42 ]
+  where
+    nrows = rows mat
+    ncols = cols mat
+
 day03 :: IO ()
 day03 = do
   inputLines <- lines <$> (getDataFileName "day03-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 03"
+  let inputInts = map (map (\c -> read [c] :: Double)) inputLines
+      a = fromLists inputInts
+      -- a = listArray ((0, 0), (length inputLines - 1, length (head inputLines) - 1)) (concat inputInts) :: Array (Int, Int) Int
+      filtro = fromLists [[1.0, 1.0, 1.0], [1.0, 0.0, 1.0], [1.0, 1.0, 1.0]] :: Matrix Double
+      b = conv2 filtro a
+  print $ sum [a ! i ! j | (i, j) <- filtradoCuarentaYDos b]
+ 
